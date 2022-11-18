167ec969e1c5c7a38468c0fdd30e05c14e125022
TOSHIBA <git config --global user.email "demirel.zekeriya@gmail.com>
2022-11-18 22:10:44 +0200

    KDV Hesap Programi

--- src/KDV_Hesap_Programi.java
+++ src/KDV_Hesap_Programi.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class KDV_Hesap_Programi {
     public static void main(String[] args) {
-        double tutar, kdvOran1 = 0.08, kdvOran2=0.18, toplam;
+        double tutar, kdvOranYuzdeSekiz = 0.08, kdvOranYuzdeOnSekiz=0.18,
+                toplam,TutarYuzdeSekiz,TutarYuzdeOnSekiz;
 
         Scanner oku=new Scanner(System.in);
         System.out.print("Ucret tutarını giriniz= ");
         tutar= oku.nextDouble();
 
-        toplam= tutar >=1000 ? (tutar+tutar*kdvOran1) : (tutar+tutar*kdvOran2);
+       TutarYuzdeSekiz=tutar+tutar*kdvOranYuzdeSekiz;
+       TutarYuzdeOnSekiz= tutar+tutar*kdvOranYuzdeOnSekiz;
+
+        toplam= tutar >=1000 ? TutarYuzdeSekiz : TutarYuzdeOnSekiz;
 
         System.out.println("KDV' siz Tutar = " + tutar);
         System.out.println("Toplam KDV' li Tutar = " + toplam);
