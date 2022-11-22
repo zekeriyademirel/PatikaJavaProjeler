bc1196408b591e9bbcfdeb843fbb66d7a294ffe2
TOSHIBA <git config --global user.email "demirel.zekeriya@gmail.com>
2022-11-22 15:11:01 +0200

    Artik Yil Hesaplama

---
+++ src/ArtikYilHesaplama.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ArtikYilHesaplama {
+    public static void main(String[] args) {
+
+        int yil;
+
+        Scanner oku=new Scanner(System.in);
+        System.out.print("Yıl Giriniz= ");
+        yil=oku.nextInt();
+
+        if (yil%4==0){
+            System.out.print(yil+ " bir artık yıldır !");
+        }else {
+            System.out.print(yil+ " bir artık yıl değildir !");
+        }
+    }
+}
--- src/CinZodyagiHesaplama.java
+++ src/CinZodyagiHesaplama.java
@@ -13,42 +13,18 @@ public class CinZodyagiHesaplama {
         kalan = dogumYili % 12;
 
         switch (kalan) {
-            case 0:
-                burc = " Maymun";
-                break;
-            case 1:
-                burc = "Horoz";
-                break;
-            case 2:
-                burc = "Köpek";
-                break;
-            case 3:
-                burc = "Domuz";
-                break;
-            case 4:
-                burc = "Fare";
-                break;
-            case 5:
-                burc = "Öküz";
-                break;
-            case 6:
-                burc = "Kaplan";
-                break;
-            case 7:
-                burc = "Tavşan";
-                break;
-            case 8:
-                burc = "Ejderha";
-                break;
-            case 9:
-                burc = "Yılan";
-                break;
-            case 10:
-                burc = "At";
-                break;
-            case 11:
-                burc = "Koyun";
-                break;
+            case 0: burc = " Maymun"; break;
+            case 1: burc = "Horoz"; break;
+            case 2: burc = "Köpek"; break;
+            case 3: burc = "Domuz"; break;
+            case 4: burc = "Fare"; break;
+            case 5: burc = "Öküz"; break;
+            case 6: burc = "Kaplan"; break;
+            case 7: burc = "Tavşan"; break;
+            case 8: burc = "Ejderha"; break;
+            case 9: burc = "Yılan"; break;
+            case 10: burc = "At"; break;
+            case 11: burc = "Koyun"; break;
         }
         System.out.print("Çin Zodyağı Burcunuz= " + burc);
     }
