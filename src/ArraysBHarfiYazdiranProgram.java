f513a1ff8bd4c70f1723e6d62d1c5600aea46958
TOSHIBA <git config --global user.email "demirel.zekeriya@gmail.com>
2022-12-25 21:31:43 +0200

    Sayi Tahmin Oyunu

--- src/ArraysBHarfiYazdiranProgram.java
+++ src/ArraysBHarfiYazdiranProgram.java
@@ -2,7 +2,6 @@ public class ArraysBHarfiYazdiranProgram {
     public static void main(String[] args) {
 
         String[][] letter = new String[7][4];
-
         for (int i = 0; i < letter.length; i++) {
             for (int j = 0; j < letter[i].length; j++) {
                 if (i == 0 || i == 3 || i==6) {
